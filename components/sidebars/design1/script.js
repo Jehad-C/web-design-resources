012f1b864c164fbe7a0f6a12b82e6518993d13f0
Jehad Cemanes <jehadcemanesece@gmail.com>
2024-09-03 16:10:04 +0800

    Add new sidebar design

---
+++ components/sidebars/design1/script.js
@@ -0,0 +1,4 @@
+document.getElementById("toggle").addEventListener("click", function () {
+    document.getElementById("sidebar").classList.toggle("sidebar-show");
+    document.getElementById("toggle").classList.toggle("toggle-on");
+})
